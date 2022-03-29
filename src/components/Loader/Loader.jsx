6b2718f33f8a080d679ae333ca66351d8733c3cd
Oleksandr Yakob <yakoboleksandr@gmail.com>
2022-03-29 15:11:59 +0300

    feat: add text loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,9 +2,10 @@ import { Oval } from "react-loader-spinner";
 
 const Loader = () => {
   return (
-    <>
+    <div style={{ display: "flex", alignItems: "center" }}>
       <Oval heigth="40" width="40" color="orange" />
-    </>
+      <h2>Loading...</h2>
+    </div>
   );
 };
 
